ac16b6351289c23aaa69c56cb74ea30354e5445c
Pedro Augusto Mendes e Silva <pedro.e@ufv.br>
2019-08-15 14:15:52 -0300

    remove test for allowed origin

--- ecommerce/tests/Feature/CorsTest.php
+++ ecommerce/tests/Feature/CorsTest.php
@@ -21,18 +21,4 @@ class CorsTest extends TestCase
         $response->assertStatus(403);
         $response->assertSeeText("Not allowed in CORS policy");
     }
-
-    /**
-     * check if route user is allowed by cors
-     * @test
-     * @return void
-     */
-    public function routeUserIsAllowedTest()
-    {
-        config(['cors.allowedOrigins' => ['test.com']]);
-
-        $response = $this->get('/api/v1/users', ['HTTP_ORIGIN' => 'test.com']);
-
-        $response->assertStatus(200);
-    }
 }
