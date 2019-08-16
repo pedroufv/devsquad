ad3896643eac58d8e9164e6a14f0467aebe1b5f7
Pedro Augusto Mendes e Silva <pedro.e@ufv.br>
2019-08-16 15:30:24 -0300

    create command and notification for import schedule

---
+++ ecommerce/app/Console/Commands/ImportProducts.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use App\Notifications\ProductsImported;
+use App\Repositories\Interfaces\ProductRepository;
+use App\Services\ProductService;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
+
+class ImportProducts extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'import:products';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Import produts on CSV files';
+
+    /**
+     * @var ProductRepository
+     */
+    protected $repository;
+
+    /**
+     * Create a new command instance.
+     *
+     * @param  ProductRepository  $repository
+     */
+    public function __construct(ProductRepository $repository)
+    {
+        $this->repository = $repository;
+
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        // get all files on directory 'storage/app/imports-scheduled'
+        if(!$paths = Storage::files(storage_path('app/imports-scheduled')))
+            return;
+
+        // import each file
+        foreach ($paths as $path) {
+            $total = ProductService::import($this->repository, $path);
+
+            // is possible to save this on a table and have the history for this
+            $name = explode('.', $path);
+
+            if(!$user = User::find($name[0]))
+                return;
+
+            // send e-mail for each user
+            $user->notify(new ProductsImported($total));
+        }
+
+    }
+}
---
+++ ecommerce/app/Notifications/ProductsImported.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class ProductsImported extends Notification
+{
+    use Queueable;
+
+    private $total;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @param  int  $total
+     */
+    public function __construct(int $total)
+    {
+        $this->total = $total;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->line('Importation concluded successfully.')
+                    ->action('Verify the list of products', url('/products'))
+                    ->line("Importeds: {$this->total}");
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
