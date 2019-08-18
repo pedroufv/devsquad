994b14f8217ef5f7b3238112f5fcabc1d3c7bfc1
Pedro Augusto <pams.pedro@gmail.com>
2019-08-18 20:32:15 -0300

    set color of bg subtitle

--- resources/views/landing/partials/header/subtitle.blade.php
+++ resources/views/landing/partials/header/subtitle.blade.php
@@ -1,6 +1,8 @@
-<div class="subtitle container">
-    <div class="row justify-content-md-center">
-        <p class="getting col-md-4">Getting <span>capital</span> for your business is complicated</p>
-        <p class="col-md-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+<div class="subtitle container-fluid">
+    <div class="container">
+        <div class="row justify-content-md-center">
+            <p class="getting col-md-4">Getting <span>capital</span> for your business is complicated</p>
+            <p class="col-md-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
     </div>
 </div>
