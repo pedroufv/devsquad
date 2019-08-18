52fc20166937a61c1ea45698ef4fb3e36335ca3e
Pedro Augusto <pams.pedro@gmail.com>
2019-08-18 19:17:50 -0300

    style a text-small into banner

--- resources/views/landing/partials/header/banner.blade.php
+++ resources/views/landing/partials/header/banner.blade.php
@@ -1,8 +1,10 @@
-<div class="banner container-fluid">
-    <div class="container">
-        <div class="called col-md-6">Fast and simple, Unsecured funding.</div>
-        <p class="col-md-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
-        <button class="button">APPLY NOW</button>
-        <p class="text-small">Applying doesn’t affect your credit score, and there’s no obligation to accept funding.</p>
+<div class="banner">
+    <div class="container-fluid">
+        <div class="container">
+            <div class="called col-md-6">Fast and simple, Unsecured funding.</div>
+            <p class="col-md-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+            <button class="button">APPLY NOW</button>
+            <p class="text-small col-md-3">Applying doesn’t affect your credit score, and there’s no obligation to accept funding.</p>
+        </div>
     </div>
 </div>
