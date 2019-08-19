1cc5678568623f27df8d67acc84a719bb6e8041d
Pedro Augusto <pams.pedro@gmail.com>
2019-08-18 22:37:49 -0300

    create startup funding section on capital for and ajustes reusable titles and buttons

--- resources/views/landing/partials/content/capitalfor.blade.php
+++ resources/views/landing/partials/content/capitalfor.blade.php
@@ -2,4 +2,13 @@
     <div class="row justify-content-center">
         <h2 class="col-md-5">Capital for start-ups, small businesses and entrepreneurs</h2>
     </div>
+    <div class="startup row">
+        <img class="img-thumbnail offset-2 col-md-3" src="{{asset('images/startup.svg')}}" alt="quickly">
+        <div class="text col-md-4">
+            <h3>Start-up funding</h3>
+            <p>Maximize your funding</p>
+            <p>Receive more funding than you could anywhere else with our strategic, perfectly-timed application process.</p>
+            <button class="button">APPLY NOW</button>
+        </div>
+    </div>
 </div>
