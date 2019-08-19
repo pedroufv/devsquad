10fbf9d26a32a76fc1bdb402d2da594a9cdd67c0
Pedro Augusto <pams.pedro@gmail.com>
2019-08-18 22:43:49 -0300

    create business funding section on capital for

--- resources/views/landing/partials/content/capitalfor.blade.php
+++ resources/views/landing/partials/content/capitalfor.blade.php
@@ -3,12 +3,21 @@
         <h2 class="col-md-5">Capital for start-ups, small businesses and entrepreneurs</h2>
     </div>
     <div class="startup row">
-        <img class="img-thumbnail offset-2 col-md-3" src="{{asset('images/startup.svg')}}" alt="quickly">
+        <img class="img-thumbnail offset-2 col-md-3" src="{{asset('images/startup.svg')}}" alt="startup">
         <div class="text col-md-4">
             <h3>Start-up funding</h3>
             <p>Maximize your funding</p>
             <p>Receive more funding than you could anywhere else with our strategic, perfectly-timed application process.</p>
             <button class="button">APPLY NOW</button>
         </div>
+    </div>
+    <div class="business row">
+        <div class="text offset-2 col-md-3">
+            <h3>Business funding</h3>
+            <p>Get lenders to compete for you</p>
+            <p>Receive better rates and higher funding amounts than you can anywhere else with our competitive loan marketplace.</p>
+            <button class="button">APPLY NOW</button>
+        </div>
+        <img class="img-thumbnail col-md-4" src="{{asset('images/business.svg')}}" alt="business">
     </div>
 </div>
