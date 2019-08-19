6fced91d0a187ba092f0558413f1f745c991849d
Pedro Augusto <pams.pedro@gmail.com>
2019-08-18 22:02:34 -0300

    create advantages

--- resources/views/landing/partials/content/guesswork.blade.php
+++ resources/views/landing/partials/content/guesswork.blade.php
@@ -5,22 +5,30 @@
     <div class="row justify-content-center">
         <p class="col-md-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
     </div>
-    <div class="row">
+    <div class="advantages row">
         <div class="quickly col-md-3">
-            <div class="img-quickly"></div>
-            <div class="txt-quickly"></div>
+            <div class="row align-items-center">
+                <img class="img-quickly img-thumbnail col-md-4" src="{{asset('images/quickly.svg')}}" alt="quickly">
+                <div class="text-advantages txt-quickly col-md-8">Funding awarded in just 10 - 21 days so you can move quickly.</div>
+            </div>
         </div>
         <div class="senior col-md-3">
-            <div class="img-senior"></div>
-            <div class="txt-senior"></div>
+            <div class="row align-items-center">
+                <img class="img-senior img-thumbnail col-md-4" src="{{asset('images/senior.svg')}}" alt="senior">
+                <div class="text-advantages txt-senior col-md-8">Senior funding advisor to help you succeed.</div>
+            </div>
         </div>
         <div class="strategize col-md-3">
-            <div class="img-strategize"></div>
-            <div class="txt-strategize"></div>
+            <div class="row align-items-center">
+                <img class="img-strategize img-thumbnail col-md-4" src="{{asset('images/strategize.svg')}}" alt="strategize">
+                <div class="text-advantages txt-strategize col-md-8">Data-driven algorithm to strategize your application process for the best results.</div>
+            </div>
         </div>
         <div class="loan col-md-3">
-            <div class="img-loan"></div>
-            <div class="txt-loan"></div>
+            <div class="row align-items-center">
+                <img class="img-loan img-thumbnail col-md-4" src="{{asset('images/loan.svg')}}" alt="loan">
+                <div class="text-advantages txt-loan col-md-8">Loan marketplace to make lenders compete for you and improve their terms.</div>
+            </div>
         </div>
     </div>
 </div>
