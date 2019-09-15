975d42f540a7a86042d6f71cf1849b5d31b0448f
Pedro Augusto <pams.pedro@gmail.com>
2019-09-14 22:11:25 -0300

    create subscription

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -7,7 +7,6 @@
 
         <title>Landing</title>
 
-        <!-- Fonts -->
         <link href="{{ asset('css/landing.css') }}" rel="stylesheet"/>
 
     </head>
@@ -26,17 +25,21 @@
                 <div class="main-message col-6">
                     <h1 class="display-4 font-weight-bold pt-5">Fast and simple, Unsecured funding.</h1>
                     <p class="col-lg-5 col-xl-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
-                    <button class="btn font-weight-bolder text-uppercase pr-5 pl-5">apply now</button>
+                    <button class="btn bg-secondary font-weight-bolder text-uppercase pr-5 pl-5">apply now</button>
                     <p class="col-lg-5 col-xl-5 font-weight-light text-small">Applying doesn’t affect your credit score, and there’s no obligation to accept funding.</p>
                 </div>
             </div>
+        </div>
 
+        <div class="container subscription">
+            <div class="row">
+                <h2 class="offset-1 col-5 font-weight-bold">Getting <span class="text-secondary">capital</span> for your business is complicated</h2>
+                <p class="col-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
         </div>
 
         <footer>
 
         </footer>
-
-
     </body>
 </html>
